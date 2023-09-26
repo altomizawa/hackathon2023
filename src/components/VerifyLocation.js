e022d3671773c48765e996902dcefd06d5a45de6
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-09-26 10:00:48 -0300

    feat(animation): add animation when clicking search button

--- src/components/VerifyLocation.js
+++ src/components/VerifyLocation.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import bairros from "../data/bairros";
 
 function VerifyLocation() {
   console.log(bairros);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = (e) => {
+    setIsClicked(true);
+  };
   return (
     <section className="verify-location">
-      <div className="verify-location__form-wrapper">
+      <div
+        className={
+          isClicked
+            ? "verify-location__form-wrapper animation__next"
+            : "verify-location__form-wrapper"
+        }
+        onAnimationEnd={() => setIsClicked(false)}
+      >
         <h2 className="verify-location__title">Selecione um bairro:</h2>
         <select className="verify-location__dropdown">
           {bairros.map((bairro, i) => (
@@ -14,7 +26,9 @@ function VerifyLocation() {
             </option>
           ))}
         </select>
-        <button className="button__search">Pesquisar</button>
+        <button onClick={handleClick} className="button__search">
+          Pesquisar
+        </button>
       </div>
     </section>
   );
