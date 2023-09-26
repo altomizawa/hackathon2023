5d32698a70cff66b1d51492f6ef7e81e43a338c3
altomizawa <alyssontomizawa@hotmail.com>
2023-09-25 23:56:53 -0300

    create bairros.js as object array

--- src/components/VerifyLocation.js
+++ src/components/VerifyLocation.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import bairros from '../data/bairros';
 
 function VerifyLocation() {
+  console.log(bairros);
   return (
     <section className="verify-location">
       <div className="verify-location__form-wrapper">
         <h2 className="verify-location__title">Selecione um bairro:</h2>
         <select className="verify-location__dropdown">
-          <option value="Vila Madalena">Vila Madalena</option>
-          <option value="Vila Madalena">Vila Mariana</option>
-          <option value="Vila Madalena">Pinheiros</option>
-          <option value="Vila Madalena">Moema</option>
-          <option value="Vila Madalena">Jardins</option>
+          <option value={bairros[0].Name}>{bairros[0].Name}</option>
+          <option value={bairros[1].Name}>{bairros[1].Name}</option>
+          <option value={bairros[2].Name}>{bairros[2].Name}</option>
+          <option value={bairros[3].Name}>{bairros[3].Name}</option>
+          <option value={bairros[4].Name}>{bairros[4].Name}</option>
+          <option value={bairros[5].Name}>{bairros[5].Name}</option>
+          <option value={bairros[6].Name}>{bairros[6].Name}</option>
+          <option value={bairros[7].Name}>{bairros[7].Name}</option>
         </select>
       </div>
     </section>
---
+++ src/data/bairros.js
@@ -0,0 +1,12 @@
+const bairros = [
+  { Name: 'Moema', Zona: 'Sul', MurderRate: 500 },
+  { Name: 'Pinheiros', Zona: 'Oeste', MurderRate: 200 },
+  { Name: 'Vila Madalena', Zona: 'Oeste', MurderRate: 500 },
+  { Name: 'Paraíso', Zona: 'Centro', MurderRate: 200 },
+  { Name: 'Jacarepaguá', Zona: 'Sul', MurderRate: 500 },
+  { Name: 'Ibirapuera', Zona: 'Sul', MurderRate: 200 },
+  { Name: 'Vila Mariana', Zona: 'Oeste', MurderRate: 500 },
+  { Name: 'Faria Lima', Zona: 'Oeste', MurderRate: 200 },
+];
+
+export default bairros;
