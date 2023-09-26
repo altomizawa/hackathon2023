5d87ddc3efca212348e73e2c6cb4ab354801507a
altomizawa <alyssontomizawa@hotmail.com>
2023-09-25 21:28:57 -0300

    redesign about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import AboutApp from './components/AboutApp';
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <AboutApp />
     </>
   );
 }
---
+++ src/components/AboutApp.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function AboutApp() {
+  return (
+    <div className="about">
+      <div className="about__title-wrapper">
+        <h1 className="about__title">O QUE É?</h1>
+        <h4 className="about__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet at
+          enim id convallis. Etiam porttitor orci risus, ut viverra dolor auctor
+          sit amet. Proin accumsan ullamcorper leo auctor aliquet. Fusce non
+          nunc nec magna molestie sagittis. Sed faucibus semper orci, eget
+          feugiat augue rhoncus vel. Etiam convallis non ipsum vitae placerat.
+          Donec non ipsum vel nibh placerat condimentum ac in mi. Integer id
+          elit a dui tincidunt mollis. Proin ornare quam in enim suscipit, in
+          efficitur nulla dignissim. Etiam dictum bibendum nisl, vel bibendum
+          libero posuere nec. Pellentesque habitant morbi tristique senectus et
+          netus et malesuada fames ac turpis egestas. Fusce gravida facilisis
+          hendrerit. Aliquam eget auctor nunc. Vivamus et maximus mi. Nunc
+          dapibus convallis ornare. Donec et scelerisque tellus.
+        </h4>
+      </div>
+    </div>
+  );
+}
+
+export default AboutApp;
