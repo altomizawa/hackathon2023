a6f68dd061657e662e0f94a5a571d460764242a9
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-09-26 01:21:17 -0300

    refact(data): use declarative approach to render neighborhoods

--- src/components/VerifyLocation.js
+++ src/components/VerifyLocation.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import bairros from '../data/bairros';
+import React from "react";
+import bairros from "../data/bairros";
 
 function VerifyLocation() {
   console.log(bairros);
@@ -8,15 +8,13 @@ function VerifyLocation() {
       <div className="verify-location__form-wrapper">
         <h2 className="verify-location__title">Selecione um bairro:</h2>
         <select className="verify-location__dropdown">
-          <option value={bairros[0].Name}>{bairros[0].Name}</option>
-          <option value={bairros[1].Name}>{bairros[1].Name}</option>
-          <option value={bairros[2].Name}>{bairros[2].Name}</option>
-          <option value={bairros[3].Name}>{bairros[3].Name}</option>
-          <option value={bairros[4].Name}>{bairros[4].Name}</option>
-          <option value={bairros[5].Name}>{bairros[5].Name}</option>
-          <option value={bairros[6].Name}>{bairros[6].Name}</option>
-          <option value={bairros[7].Name}>{bairros[7].Name}</option>
+          {bairros.map((bairro, i) => (
+            <option key={i + 1} value={bairro.Name}>
+              {bairro.Name}
+            </option>
+          ))}
         </select>
+        <button className="button__search">Pesquisar</button>
       </div>
     </section>
   );
