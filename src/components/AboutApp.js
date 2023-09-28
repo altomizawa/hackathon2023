a14f8390841779a0e8c79b9ec87b783b52356d82
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-09-28 01:02:55 -0300

    feat(media): add media queries to about section

--- src/components/AboutApp.js
+++ src/components/AboutApp.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function AboutApp() {
   return (
     <div className="about" id="about-app">
-      <div className="about__title-wrapper">
-        <h1 className="about__title">O QUE É?</h1>
-        <h4 className="about__text">
-          O AM I SAFE? é um aplicativo que visa ajudar todas as pessoas que não
-          conhecem a cidade de São Paulo a se sentirem mais seguras quando
-          estiverem lá, seja visitando ou a trabalho. Baseado em um banco de
-          dados de 7 meses para trás, o aplicativo é capaz de dizer qual o nível
-          de segurança de um bairro específico. O funcionamento é simples: basta
-          escolher o bairro que você deseja pesquisar e o aplicativo
-          imediatamente te retornará com uma pontuação de 'segurança' e ainda te
-          sugerirá outras opções mais seguras, caso sua opção não seja.
-        </h4>
-      </div>
+      <h1 className="about__title">O QUE É?</h1>
+      <h4 className="about__text">
+        O AM I SAFE? é um aplicativo que visa ajudar todas as pessoas que não
+        conhecem a cidade de São Paulo a se sentirem mais seguras quando
+        estiverem lá, seja visitando ou a trabalho. Baseado em um banco de dados
+        de 7 meses para trás, o aplicativo é capaz de dizer qual o nível de
+        segurança de um bairro específico. O funcionamento é simples: basta
+        escolher o bairro que você deseja pesquisar e o aplicativo imediatamente
+        te retornará com uma pontuação de 'segurança' e ainda te sugerirá outras
+        opções mais seguras, caso sua opção não seja.
+      </h4>
     </div>
   );
 }
